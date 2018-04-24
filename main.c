077f04b4a35b5868505ca264f595c13a506ceb6b
James Lovejoy <jameslovejoy1@gmail.com>
2018-04-24 19:15:28 -0400

    Very sloppy tests

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int run() {
         return -1;
     }
 
-    schnorr_key* key = schnorr_key_new(ctx);
+    /*schnorr_key* key = schnorr_key_new(ctx);
     if(key == NULL) {
         return -1;
     }
@@ -19,16 +19,16 @@ int run() {
 
     if(schnorr_verify(ctx, sig, key->pub, "hello", 5) != 1) {
         return -1;
-    }
+    }*/
     
 
 
     committed_r_key* rkey = committed_r_key_new(ctx);
-    if(key == NULL) {
+    if(rkey == NULL) {
         return -1;
     }
 
-    committed_r_sig* rsig;
+    /*committed_r_sig* rsig;
     if(committed_r_sign(ctx, &rsig, rkey, "hello", 5) == 0) {
         return -1;
     }
@@ -60,9 +60,9 @@ int run() {
 
     if(schnorr_verify(ctx, forgery, &convPub, "random", 6) != 1) {
         return -1;
-    }
+    }*/
 
-    committed_r_key* rkey2 = committed_r_key_new(ctx);
+    /*committed_r_key* rkey2 = committed_r_key_new(ctx);
     if(rkey2 == NULL) {
         return -1;
     }
@@ -77,8 +77,18 @@ int run() {
 
     schnorr_sig* sig1;
     schnorr_sig* sig2;
-    schnorr_pubkey* pub;
-    if(musig_sign(ctx, &sig1, &pub, keys[0], pubkeys, 1, "hello", 5) == 0) {
+    schnorr_pubkey* pub;*/
+
+    musig_key* key = musig_key_new(ctx);
+    musig_key* keys[1];
+    keys[0] = key;
+
+    musig_pubkey* pubkeys[1];
+    pubkeys[0] = key->pub;
+
+    musig_sig* sig;
+    musig_pubkey* pub;
+    if(musig_sign(ctx, &sig, &pub, keys[0], pubkeys, 1, "hello", 5) == 0) {
         return -1;
     }
 
@@ -86,28 +96,30 @@ int run() {
         return -1;
     }*/
 
-    schnorr_sig* sigs[2];
+    /*schnorr_sig* sigs[2];
     sigs[0] = sig1;
-    sigs[1] = sig2;
+    sigs[1] = sig2;*/
 
     /*schnorr_sig* sigAgg;
     if(musig_aggregate(ctx, &sigAgg, sigs, 2) == 0) {
         return -1;
     }*/
 
-    if(musig_verify(ctx, sig1, pub, "hello", 5) != 1) {
+    if(musig_verify(ctx, sig, pub, "hello", 5) != 1) {
+        char* sHex = BN_bn2hex(sig->s);
+        char* AHex = EC_POINT_point2hex(ctx->group, pub->A, POINT_CONVERSION_COMPRESSED, ctx->bn_ctx);
+        char* RHex = EC_POINT_point2hex(ctx->group, sig->R, POINT_CONVERSION_COMPRESSED, ctx->bn_ctx);
+
+        printf("s: %s, X: %s, R: %s\n", sHex, AHex, RHex);
         return -1;
     }
 
-    committed_r_key_free(rkey);
-    committed_r_key_free(rkey2);
-    committed_r_key_free(recovered);
-    committed_r_sig_free(rsig);
-    committed_r_sig_free(rsig2);
-    schnorr_sig_free(sig);
-    schnorr_key_free(key);
-    schnorr_sig_free(forgery);
-    schnorr_sig_free(sig1);
+    //committed_r_key_free(recovered);
+    //committed_r_sig_free(rsig);
+    //committed_r_sig_free(rsig2);
+    //schnorr_sig_free(sig);
+    //schnorr_key_free(key);
+    ///schnorr_sig_free(forgery);
     //schnorr_sig_free(sig2);
     //schnorr_sig_free(sigAgg);
     EC_POINT_free(pub->A);
