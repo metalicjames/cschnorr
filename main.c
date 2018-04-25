5a9deb9640a9bcd4779cdb370eb83292084b6140
James Lovejoy <jameslovejoy1@gmail.com>
2018-04-25 15:47:35 -0400

    Add back all the tests, committed_r_recover is sometimes failing

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ int run() {
         return -1;
     }
 
-    /*schnorr_key* key = schnorr_key_new(ctx);
+    schnorr_key* key = schnorr_key_new(ctx);
     if(key == NULL) {
         return -1;
     }
@@ -19,16 +19,14 @@ int run() {
 
     if(schnorr_verify(ctx, sig, key->pub, "hello", 5) != 1) {
         return -1;
-    }*/
-    
-
+    }
 
     committed_r_key* rkey = committed_r_key_new(ctx);
     if(rkey == NULL) {
         return -1;
     }
 
-    /*committed_r_sig* rsig;
+    committed_r_sig* rsig;
     if(committed_r_sign(ctx, &rsig, rkey, "hello", 5) == 0) {
         return -1;
     }
@@ -60,68 +58,53 @@ int run() {
 
     if(schnorr_verify(ctx, forgery, &convPub, "random", 6) != 1) {
         return -1;
-    }*/
-
-    /*committed_r_key* rkey2 = committed_r_key_new(ctx);
-    if(rkey2 == NULL) {
-        return -1;
     }
 
-    committed_r_key* keys[2];
-    keys[0] = rkey;
-    keys[1] = rkey2;
+    musig_key* key1 = musig_key_new(ctx);
+    musig_key* key2 = musig_key_new(ctx);
+    musig_key* keys[2];
+    keys[0] = key1;
+    keys[1] = key2;
 
-    committed_r_pubkey* pubkeys[2];
-    pubkeys[0] = rkey->pub;
-    pubkeys[1] = rkey2->pub;
+    musig_pubkey* pubkeys[2];
+    pubkeys[0] = key1->pub;
+    pubkeys[1] = key2->pub;
 
-    schnorr_sig* sig1;
-    schnorr_sig* sig2;
-    schnorr_pubkey* pub;*/
-
-    musig_key* key = musig_key_new(ctx);
-    musig_key* keys[1];
-    keys[0] = key;
-
-    musig_pubkey* pubkeys[1];
-    pubkeys[0] = key->pub;
-
-    musig_sig* sig;
+    musig_sig* sig1;
+    musig_sig* sig2;
     musig_pubkey* pub;
-    if(musig_sign(ctx, &sig, &pub, keys[0], pubkeys, 1, "hello", 5) == 0) {
+    if(musig_sign(ctx, &sig1, &pub, keys[0], pubkeys, 2, "hello", 5) == 0) {
         return -1;
     }
 
-    /*if(musig_sign(ctx, &sig2, &pub, keys[1], pubkeys, 2, "hello", 5) == 0) {
+    if(musig_sign(ctx, &sig2, &pub, keys[1], pubkeys, 2, "hello", 5) == 0) {
         return -1;
-    }*/
+    }
 
-    /*schnorr_sig* sigs[2];
+    musig_sig* sigs[2];
     sigs[0] = sig1;
-    sigs[1] = sig2;*/
+    sigs[1] = sig2;
 
-    /*schnorr_sig* sigAgg;
+    musig_sig* sigAgg;
     if(musig_aggregate(ctx, &sigAgg, sigs, 2) == 0) {
         return -1;
-    }*/
+    }
 
-    if(musig_verify(ctx, sig, pub, "hello", 5) != 1) {
+    if(musig_verify(ctx, sigAgg, pub, "hello", 5) != 1) {
         char* sHex = BN_bn2hex(sig->s);
         char* AHex = EC_POINT_point2hex(ctx->group, pub->A, POINT_CONVERSION_COMPRESSED, ctx->bn_ctx);
-        char* RHex = EC_POINT_point2hex(ctx->group, sig->R, POINT_CONVERSION_COMPRESSED, ctx->bn_ctx);
+        char* RHex = EC_POINT_point2hex(ctx->group, sigAgg->R, POINT_CONVERSION_COMPRESSED, ctx->bn_ctx);
 
         printf("s: %s, X: %s, R: %s\n", sHex, AHex, RHex);
         return -1;
     }
 
-    //committed_r_key_free(recovered);
-    //committed_r_sig_free(rsig);
-    //committed_r_sig_free(rsig2);
-    //schnorr_sig_free(sig);
-    //schnorr_key_free(key);
-    ///schnorr_sig_free(forgery);
-    //schnorr_sig_free(sig2);
-    //schnorr_sig_free(sigAgg);
+    committed_r_key_free(recovered);
+    committed_r_sig_free(rsig);
+    committed_r_sig_free(rsig2);
+    schnorr_sig_free(sig);
+    schnorr_key_free(key);
+    schnorr_sig_free(forgery);
     EC_POINT_free(pub->A);
     free(pub);
     schnorr_context_free(ctx);
