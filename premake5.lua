fc0d6f201c111710fa7e65a6102d35d9e90f7e44
James Lovejoy <jameslovejoy1@gmail.com>
2018-07-02 14:58:07 -0400

    Replace build shell script with premake5 configuration

---
+++ premake5.lua
@@ -0,0 +1,43 @@
+newoption {
+    trigger = "lib-dir",
+    description = "Specify extra directory to look for libraries in",
+    value = "DIR"
+}
+
+newoption {
+    trigger = "include-dir",
+    description = "Specify extra directory to look for headers in",
+    value = "DIR"
+}
+
+workspace "cschnorr"
+    configurations {"Debug", "Release"}
+    platforms {"Static", "Shared"}
+    language "C"
+    includedirs {_OPTIONS["include-dir"]}
+    libdirs {_OPTIONS["lib-dir"]}
+
+    filter {"configurations:Debug"}
+        optimize "Off"
+        symbols "Off"
+
+    filter {"configurations:Release"}
+        optimize "Full"
+        symbols "On"
+
+project "cschnorr"
+    
+    files {"src/**.h", "src/**.c"}
+
+    filter {"platforms:Static"}
+        kind "StaticLib"
+
+    filter {"platforms:Shared"}
+        kind "SharedLib"
+
+project "test"
+
+    kind "ConsoleApp"
+    files {"main.c"}
+    links {"cschnorr", "crypto"}
+    
